964d23f83b74c63f0b0dfd5be44991caf6c238e4
Jose Antonio Vázquez <wistonbogarde@gmail.com>
2024-12-05 16:26:26 +0100

    Add files via upload

---
+++ palabras.js
@@ -0,0 +1,50 @@
+// Declaro array de objeto con la palabra y la pista.
+const palabras = [{
+    palabra: "bicycle",
+    pista: "You ride it with two wheels"
+},{
+    palabra: "elephant",
+    pista: "A large animal with a trunk"
+},{
+    palabra: "rainbow",
+    pista: "Colors in the sky after rain"
+},{
+    palabra: "library",
+    pista: "Place with lots of books"
+},{
+    palabra: "soccer",
+    pista: "A sport played with a ball and goals"
+},{
+    palabra: "sunflower",
+    pista: "A tall plant with a big yellow flower"
+},{
+    palabra: "mountain",
+    pista: "A very high landform"
+},{
+    palabra: "pencil",
+    pista: "You use it to write or draw"
+},{
+    palabra: "castle",
+    pista: "A large building where kings and queens live"
+},{
+    palabra: "island",
+    pista: "Land surrounded by water"
+},{
+    palabra: "giraffe",
+    pista: "A tall animal with a long neck"
+},{
+    palabra: "rocket",
+    pista: "A vehicle that goes to space"
+},{
+    palabra: "butterfly",
+    pista: "An insect with colorful wings"
+},{
+    palabra: "penguin",
+    pista: "A bird that can't fly and lives in the cold"
+},{
+    palabra: "bridge",
+    pista: "A structure that allows people to cross over water or roads"
+},{
+    palabra: "pirate",
+    pista: "A person who sails on a ship and looks for treasure"
+}]
